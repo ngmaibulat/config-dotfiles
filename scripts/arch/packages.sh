e6f81048a98636793a9a565e80e7961ad9de2099
Aibulat <ngmaibulat@gmail.com>
2024-01-10 22:53:23 +0400

    Wallpaper related functions

--- scripts/arch/packages.sh
+++ scripts/arch/packages.sh
@@ -2,3 +2,7 @@
 
 sudo pacman --noconfirm -S open-vm-tools openssh git vim neovim python zsh tmux neofetch fastfetch tldr glow git make fakeroot gcc go
 
+### X11
+
+sudo pacman -S feh --noconfirm
+
--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-cp bashrc/bashrc.sh ~/.bashrc
-cp vimrc/vimrc ~/.vimrc
+cp bashrc/bashrc.sh  ~/.bashrc
+cp vimrc/vimrc       ~/.vimrc
 
-cat functions/basic.sh >> ~/.bashrc
+cat functions/basic.sh          >> ~/.bashrc
+cat functions/get-wallpapers.sh >> ~/.bashrc
+cat functions/set-wallpaper.sh  >> ~/.bashrc
 
