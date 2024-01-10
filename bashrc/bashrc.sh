78e2fffff2a3aca441f219cbbab6de934951bba2
Aibulat <ngmaibulat@gmail.com>
2024-01-10 22:01:56 +0400

    setup script

---
+++ bashrc/bashrc.sh
@@ -0,0 +1,5 @@
+export PS1='\033[01;32m\u@\h\033[00m:\033[01;34m\w\033[00m\$ '
+
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp bashrc/bashrc.sh ~/.bashrc
+cp vimrc/vimrc ~/.vimrc
+
