502e9d9668e290a3a3eb3b3e9b17b8d7f009f046
Aibulat <ngmaibulat@gmail.com>
2024-01-11 14:50:56 +0400

    update X11 configs

--- setup.sh
+++ setup.sh
@@ -7,6 +7,8 @@ cat functions/basic.sh          >> ~/.bashrc
 cat functions/get-wallpapers.sh >> ~/.bashrc
 cat functions/set-wallpaper.sh  >> ~/.bashrc
 
+cp X11/xinitrc-qtile.sh ~/.xinitrc
+cp X11/Xresources-dpi-220 ~/.Xresources
 
 source ~/.bashrc
 
